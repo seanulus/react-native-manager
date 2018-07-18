e84cb7a944654f14e85404358cb60472bee52ca9
Sean McDermott <seanjmac@hotmail.com>
2018-07-18 15:21:21 -0700

    Added routing to employee list from login and list to create. Start employee create form.

---
+++ src/components/EmployeeCreate.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Card, CardSection, Input, Button } from './common'
+
+class EmployeeCreate extends Component {
+  render() {
+    return (
+      <Card>
+        <CardSection>
+          <Input
+            label='name'
+            placeholder='Jane'
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            label='Phone'
+            placeholder='555-555-5555'
+          />  
+        </CardSection>
+
+        <CardSection>
+        </CardSection>
+
+        <CardSection>
+          <Button>
+            Create
+          </Button>
+        </CardSection>
+      </Card>
+    );
+  }
+}
+
+export default EmployeeCreate;
