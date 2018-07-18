b7ed8e01385e5e0ef5b0e3f5489803472509757b
Sean McDermott <seanjmac@hotmail.com>
2018-07-18 16:44:31 -0700

    Add styling to picker. Debugging style override.

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Picker } from 'react-native';
+import { Picker, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { employeeUpdate } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
@@ -26,12 +26,12 @@ class EmployeeCreate extends Component {
           />
         </CardSection>
 
-        <CardSection>
+        <CardSection style={{ flexDirection: 'column' }}>
+          <Text style={styles.pickerTextStyle}>Shift</Text>
           <Picker
             selectedValue={this.props.shift}
             onValueChange={value => this.props.employeeUpdate({ prop: 'shift', value })}
-            style={{ flex: 1 }}
-          >
+            style={{ flex: 1 }}>
             <Picker.Item label='Monday' value='Monday' />
             <Picker.Item label='Tuesday' value='Tuesday' />
             <Picker.Item label='Wednesday' value='Wednesday' />
@@ -49,6 +49,13 @@ class EmployeeCreate extends Component {
         </CardSection>
       </Card>
     );
+  }
+}
+
+const styles = {
+  pickerTextStyle: {
+    fontSize: 18,
+    paddingLeft: 20
   }
 }
 
