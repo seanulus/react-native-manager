51d562acdac4d803fe59065129ae0d21c87ffc53
Sean McDermott <seanjmac@hotmail.com>
2018-07-19 15:56:06 -0700

    Add functionality to edit employee schedules

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,15 +1,29 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Card, CardSection, Button } from './common';
+import { employeeUpdate, employeeSave } from '../actions'
 import EmployeeForm from './EmployeeForm';
 
 class EmployeeEdit extends Component {
+  componentWillMount() {
+    _.each(this.props.employee, (value, prop) => {
+      this.props.employeeUpdate({ value, prop});
+    });
+  }
+
+  onButtonPress() {
+    const { name, phone, shift } = this.props;
+
+    this.props.employeeSave({ name, phone, shift, uid: this.props.employee.uid });
+  }
+
   render() {
     return (
       <Card>
         <EmployeeForm />
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Save Changes
           </Button>
         </CardSection>
@@ -18,4 +32,10 @@ class EmployeeEdit extends Component {
   }
 }
 
-export default connect()(EmployeeEdit);
+const mapStateToProps = (state) => {
+  const { name, phone, shift } = state.employeeForm;
+
+  return { name, phone, shift };
+}
+
+export default connect(mapStateToProps, { employeeUpdate, employeeSave })(EmployeeEdit);
